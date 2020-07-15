049e826437b2cfb2f3b5f0cc45589b6132dd33d7
koftov <ahronlu@gmail.com>
2020-07-15 23:01:35 +0300

    push to github

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <i-container class="container">
+      <i-row>
+        <i-column sm="6" class="border-right">
+          <h1>Markdown</h1>
+          <i-textarea v-model="text" placeholder="Enter Markdown Here" />
+        </i-column>
+        <i-column sm="6">
+          <h1>Formated Text</h1>
+          <VueMarkdown :source="text">{{ text }}</VueMarkdown>
+        </i-column>
+      </i-row>
+    </i-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VueMarkdown from 'vue-markdown';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    VueMarkdown,
+  },
+  data() {
+    return {
+      text: '',
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  margin-top: 40px;
+}
+.border-right {
+  border-right: 1px dotted;
+}
+.form-input {
+  height: 400px;
 }
 </style>
